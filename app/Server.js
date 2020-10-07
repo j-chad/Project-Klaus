28239c1043fb1b49f6550dcf52c946a6f3e19b43
Jackson Chadfield <jackson.chadfield@jacksonc.dev>
2020-10-07 14:35:18 +1300

    serve client in server

--- app/Server.js
+++ app/Server.js
@@ -1,12 +1,19 @@
 
-import {Express, Request, Response} from "express";
+import express from "express";
+import * as path from "path";
 
 export class Server {
     constructor(app) {
         this.app = app;
 
+        this.app.use(express.static(path.resolve("./") + "/build/frontend"));
+
         this.app.get("/api", (req, res) => {
             res.send("You have reached the API!");
+        });
+
+        this.app.get("*", (req, res) => {
+            res.sendFile(path.resolve("./") + "/build/frontend/index.html");
         });
     }
 
