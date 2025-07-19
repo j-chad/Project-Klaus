ca36e24a657e9646c69d44836ff93048382a829d
jackson <contact@jacksonc.dev>
2025-07-20 06:40:19 +1200

    split up subtle utils

--- frontend/src/lib/crypto/subtle.ts
+++ frontend/src/lib/crypto/subtle.ts
@@ -1,4 +1,4 @@
-const NONCE_BYTES = 12; // AES-GCM nonce size is 12 bytes
+import { combineNonceAndKey, NONCE_BYTES, securePermute, splitNonceAndKey } from './utils';
 
 export async function generatePublicationKeypair(): Promise<CryptoKeyPair> {
 	return crypto.subtle.generateKey(
@@ -17,10 +17,10 @@ export async function encryptPublicationMessage(
 	message: string,
 	publicKeys: CryptoKey[]
 ): Promise<Uint8Array> {
-	const permutedKeys = securePermutePublicKeys(publicKeys);
+	const permutedKeys = securePermute(publicKeys);
 
 	let messageBuffer: Uint8Array = new TextEncoder().encode(message);
-	const nonceBuffer = new Uint8Array(12); // AES-GCM nonce size is 12 bytes / 96 bits
+	const nonceBuffer = new Uint8Array(NONCE_BYTES);
 
 	for (const rsaKey of permutedKeys) {
 		messageBuffer = await hybridEncrypt(messageBuffer, rsaKey, nonceBuffer);
@@ -137,82 +137,4 @@ async function hybridDecrypt(
 		key,
 		cipherText
 	);
-}
-
-function combineNonceAndKey(nonce: Uint8Array, key: ArrayBuffer): Uint8Array {
-	const combined = new Uint8Array(NONCE_BYTES + key.byteLength);
-	combined.set(nonce, 0);
-	combined.set(new Uint8Array(key), NONCE_BYTES);
-	return combined;
-}
-
-function splitNonceAndKey(combined: ArrayBuffer): { nonce: ArrayBuffer; key: ArrayBuffer } {
-	const nonce = combined.slice(0, NONCE_BYTES);
-	const key = combined.slice(NONCE_BYTES);
-	return { nonce, key };
-}
-
-/**
- * Returns a cryptographically secure random integer between min and max, inclusive.
- *
- * Adapted from https://stackoverflow.com/questions/18230217/
- *
- * @param {number} min - the lowest integer in the desired range (inclusive)
- * @param {number} max - the highest integer in the desired range (inclusive)
- * @returns {number} Random number
- */
-function secureRandomInt(min: number, max: number): number {
-	const RAND_MAX_RANGE_SIZE = 2 ** 53;
-	const buffer = new Uint32Array(2 ** 10);
-
-	if (!(Number.isSafeInteger(min) && Number.isSafeInteger(max))) {
-		throw Error('min and max must be safe integers');
-	}
-
-	if (min > max) {
-		throw Error('min must be less than or equal to max');
-	}
-
-	const rangeSize = max - min + 1;
-	if (rangeSize > RAND_MAX_RANGE_SIZE) {
-		throw Error('(max - min) must be <= Number.MAX_SAFE_INTEGER');
-	}
-
-	const rejectionThreshold = RAND_MAX_RANGE_SIZE - (RAND_MAX_RANGE_SIZE % rangeSize);
-	let offset = buffer.length;
-	let result: number;
-	do {
-		if (offset + 1 >= buffer.length) {
-			crypto.getRandomValues(buffer);
-			offset = 0;
-		}
-
-		const n1 = buffer[offset++];
-		const n2 = buffer[offset++];
-		if (n1 === undefined || n2 === undefined) {
-			throw Error('failed to generate random numbers');
-		}
-
-		result = (n1 & 0x1f_ffff) * 0x1_0000_0000 + n2;
-	} while (result >= rejectionThreshold);
-	return min + (result % rangeSize);
-}
-
-/**
- * Securely permutes the order of public keys using the Fisher-Yates shuffle algorithm and a CSRNG.
- *
- * @param {CryptoKey[]} keys - An array of CryptoKey objects representing public keys.
- * @return {CryptoKey[]} A new array of CryptoKey objects with the order of keys shuffled.
- */
-function securePermutePublicKeys(keys: CryptoKey[]): CryptoKey[] {
-	// Fisher-Yates shuffle algorithm
-	const result = keys.slice();
-
-	const n = keys.length;
-	for (let i = n - 1; i > 0; i--) {
-		const j = secureRandomInt(0, i);
-		[result[i], result[j]] = [result[j], result[i]] as [CryptoKey, CryptoKey];
-	}
-
-	return result;
 }
---
+++ frontend/src/lib/crypto/utils.ts
@@ -0,0 +1,89 @@
+export const NONCE_BYTES = 12; // AES-GCM nonce size is 12 bytes
+
+/**
+ * Combines a nonce and a symmetric key into a single Uint8Array.
+ * @param nonce
+ * @param key
+ */
+export function combineNonceAndKey(nonce: Uint8Array, key: ArrayBuffer): Uint8Array {
+	const combined = new Uint8Array(NONCE_BYTES + key.byteLength);
+	combined.set(nonce, 0);
+	combined.set(new Uint8Array(key), NONCE_BYTES);
+	return combined;
+}
+
+/**
+ * Splits a combined ArrayBuffer into its nonce and key components.
+ * @param combined - The combined ArrayBuffer containing the nonce and key.
+ */
+export function splitNonceAndKey(combined: ArrayBuffer): { nonce: ArrayBuffer; key: ArrayBuffer } {
+	const nonce = combined.slice(0, NONCE_BYTES);
+	const key = combined.slice(NONCE_BYTES);
+	return { nonce, key };
+}
+
+/**
+ * Returns a cryptographically secure random integer between min and max, inclusive.
+ *
+ * Adapted from https://stackoverflow.com/questions/18230217/
+ *
+ * @param {number} min - the lowest integer in the desired range (inclusive)
+ * @param {number} max - the highest integer in the desired range (inclusive)
+ * @returns {number} Random number
+ */
+export function secureRandomInt(min: number, max: number): number {
+	const RAND_MAX_RANGE_SIZE = 2 ** 53;
+	const buffer = new Uint32Array(2 ** 10);
+
+	if (!(Number.isSafeInteger(min) && Number.isSafeInteger(max))) {
+		throw Error('min and max must be safe integers');
+	}
+
+	if (min > max) {
+		throw Error('min must be less than or equal to max');
+	}
+
+	const rangeSize = max - min + 1;
+	if (rangeSize > RAND_MAX_RANGE_SIZE) {
+		throw Error('(max - min) must be <= Number.MAX_SAFE_INTEGER');
+	}
+
+	const rejectionThreshold = RAND_MAX_RANGE_SIZE - (RAND_MAX_RANGE_SIZE % rangeSize);
+	let offset = buffer.length;
+	let result: number;
+	do {
+		if (offset + 1 >= buffer.length) {
+			crypto.getRandomValues(buffer);
+			offset = 0;
+		}
+
+		const n1 = buffer[offset++];
+		const n2 = buffer[offset++];
+		if (n1 === undefined || n2 === undefined) {
+			throw Error('failed to generate random numbers');
+		}
+
+		result = (n1 & 0x1f_ffff) * 0x1_0000_0000 + n2;
+	} while (result >= rejectionThreshold);
+	return min + (result % rangeSize);
+}
+
+/**
+ * Securely permutes the order of an array using the Fisher-Yates shuffle algorithm and a CSRNG.
+ *
+ * shuffling is done in place
+ *
+ * @template T - The type of the elements in the array.
+ * @param {T[]} array - An array of objects
+ * @return {T[]} A new array of CryptoKey objects with the order of keys shuffled.
+ */
+export function securePermute<T>(array: T[]): T[] {
+	// Fisher-Yates shuffle algorithm
+	const n = array.length;
+	for (let i = n - 1; i > 0; i--) {
+		const j = secureRandomInt(0, i);
+		[array[i], array[j]] = [array[j], array[i]] as [T, T];
+	}
+
+	return array;
+}
