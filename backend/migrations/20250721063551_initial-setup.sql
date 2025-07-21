2efdda7496c4a7adb413a4cc38f9180cb449e300
jackson <contact@jacksonc.dev>
2025-07-21 19:25:11 +1200

    add initial tables

--- backend/migrations/20250721063551_initial-setup.sql
+++ backend/migrations/20250721063551_initial-setup.sql
@@ -1,7 +1,48 @@
-CREATE TABLE Room (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TABLE room (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+
+    password_hash TEXT,
+    max_members INTEGER,
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    started_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+    deleted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL
+);
+
+CREATE TRIGGER trigger_update_room_updated_at
+    BEFORE UPDATE ON room
+    FOR EACH ROW
+EXECUTE PROCEDURE update_updated_at_column();
+
+CREATE TABLE room_member (
+    id          UUID PRIMARY KEY         DEFAULT gen_random_uuid(),
+    room_id     UUID    NOT NULL REFERENCES room(id) ON DELETE CASCADE,
+
+    fingerprint TEXT UNIQUE NOT NULL,
+    is_owner    BOOLEAN NOT NULL         DEFAULT FALSE,
+
+    joined_at   TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE session (
+    id          UUID PRIMARY KEY         DEFAULT gen_random_uuid(),
+    member_id  UUID    NOT NULL REFERENCES room_member(id) ON DELETE CASCADE,
+
+    token       TEXT UNIQUE NOT NULL,
+
+    user_agent  TEXT NOT NULL,
+    ip_address  INET,
+
+    created_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    last_seen_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
