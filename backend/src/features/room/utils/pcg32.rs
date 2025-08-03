f57ba8d39efb5d5515a36ecbc60df28341a56c1c
jackson <contact@jacksonc.dev>
2025-08-04 08:34:23 +1200

    improve random number gen

--- backend/src/features/room/utils/pcg32.rs
+++ backend/src/features/room/utils/pcg32.rs
@@ -3,6 +3,12 @@
 const MULTIPLIER: u64 = 6364136223846793005;
 const INCREMENT: u64 = 1442695040888963407;
 
+// Output function XSH RR: xorshift high (bits), followed by a random rotate
+// Constants are for 64-bit state, 32-bit output
+const ROTATE: u32 = 59; // 64 - 5
+const XSHIFT: u32 = 18; // (5 + 32) / 2
+const SPARE: u32 = 27; // 64 - 32 - 5
+
 // PCG-XSH-RR-32
 pub struct Pcg32 {
     state: u64,
@@ -23,26 +29,29 @@ impl Pcg32 {
         self.advance_state();
 
         #[allow(clippy::cast_possible_truncation)]
-        let xor_shifted = (((old_state >> 18) ^ old_state) >> 27) as u32;
-        let rot = (old_state >> 59) as u32;
+        let xor_shifted = (((old_state >> XSHIFT) ^ old_state) >> SPARE) as u32;
+        let rot = (old_state >> ROTATE) as u32;
 
         xor_shifted.rotate_right(rot)
     }
 
-    /// Generates a random number in the range [0, max) without modulo bias.
-    pub fn gen_range(&mut self, max: u32) -> u32 {
-        if max == 0 {
-            return 0;
-        }
+    #[allow(clippy::cast_possible_truncation)]
+    /// Lemire's debiased integer multiplicative generator
+    pub fn gen_range(&mut self, range: u32) -> u32 {
+        let random_value = self.next_u32();
+        let mut full_product = u64::from(random_value) * u64::from(range);
+        let mut product_low_bits = full_product as u32;
 
-        // avoid modulo bias using rejection sampling + modulo reduction
-        let threshold = u32::MAX - (u32::MAX % max);
-        loop {
-            let value = self.next_u32();
-            if value < threshold {
-                return value % max;
+        if product_low_bits < range {
+            let threshold = 0u32.wrapping_sub(range) % range;
+            while product_low_bits < threshold {
+                let random_value = self.next_u32();
+                full_product = u64::from(random_value) * u64::from(range);
+                product_low_bits = full_product as u32;
             }
         }
+
+        (full_product >> 32) as u32
     }
 
     fn advance_state(&mut self) {
