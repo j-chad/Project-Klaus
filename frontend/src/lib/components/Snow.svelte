d1c0d60138047c803a91a80cb16ba0e9b28dd4b4
jackson <contact@jacksonc.dev>
2025-08-05 06:41:39 +1200

    slight adjustments

--- frontend/src/lib/components/Snow.svelte
+++ frontend/src/lib/components/Snow.svelte
@@ -3,16 +3,6 @@
 <script lang="ts">
     import { onMount } from 'svelte'
 
-    // a bunch of variables defining the snow and how it falls
-    const SNOWFLAKES_COUNT = 70
-    const SNOWFLAKE_MIN_SCALE = 0.1
-    const MELTING_SPEED = 1.12
-    const WIND_FORCE = 0.01
-    const FALL_SPEED = 0.09
-    const TARGET_FPS = 80
-
-    const MS_BETWEEN_FRAMES = 1000 / TARGET_FPS
-
     interface Snowflake {
         scale: number
         x: number
@@ -21,10 +11,21 @@
         id: string
     }
 
+    // a bunch of variables defining the snow and how it falls
+    const SNOWFLAKES_COUNT = 200
+    const SNOWFLAKE_MIN_SCALE = 0.1
+    const SNOWFLAKE_MAX_SCALE = 0.7
+    const MELTING_SPEED = 1.12
+    const WIND_FORCE = 0.01
+    const FALL_SPEED = 0.19
+    const TARGET_FPS = 80
+
+    const MS_BETWEEN_FRAMES = 1000 / TARGET_FPS
+
     function randomSnowflakeConfig(i: number): Snowflake {
         return {
             id: `snowflake-${i}`,
-            scale: SNOWFLAKE_MIN_SCALE + Math.random() * (1 - SNOWFLAKE_MIN_SCALE),
+            scale: SNOWFLAKE_MIN_SCALE + Math.random() * SNOWFLAKE_MAX_SCALE,
             x: -20 + Math.random() * 120,
             y: -100 + Math.random() * 200,
             opacity: 0.999,
@@ -67,20 +68,11 @@
 </script>
 
 <style>
-    :global(body) {
-        min-height: 100%;
-    }
-
-    :global(html) {
-        height: 100%;
-    }
-
     .snow {
         width: 10px;
         height: 10px;
         background: white;
         border-radius: 50%;
-        box-shadow: 0 0 5px rgba(255, 255, 255, 0.5);
         position: absolute;
         z-index: 1000;
         overflow: hidden;
